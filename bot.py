581c799197d6f7169fc987035ebdce643acf1cd4
Elena Sivogolovko <efecca@gmail.com>
2018-05-26 12:39:33 +0300

    bot initial

---
+++ bot.py
@@ -0,0 +1,37 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import settings
+
+# Настройки прокси
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
+    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
+import logging
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+def main():
+    mybot = Updater(settings.TELEGRAM_API_KEY, request_kwargs=PROXY)
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    mybot.start_polling()
+    mybot.idle()
+
+def greet_user(bot, update):
+    #Задание: в функции greet_user выедите на экран содержимое переменной update
+    print('Update: {}'.format(update))
+    text = 'Вызван /start'
+    print(text)
+    update.message.reply_text(text)      
+
+def talk_to_me(bot, update):
+    user_text = update.message.text 
+    print(user_text)
+    update.message.reply_text(user_text)
+       
+
+main()
